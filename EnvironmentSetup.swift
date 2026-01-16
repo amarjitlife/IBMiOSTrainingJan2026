206487152dccac1edeec7b7849692a860b774de2
Amarjit Singh <amarjit.developer@gmail.com>
2026-01-16 10:47:52 +0530

    Swift Environment Setup Instructions
    Add instructions for setting up the environment on Windows.

---
+++ EnvironmentSetup.swift
@@ -0,0 +1,24 @@
+
+// In Windows
+
+// 1. Create Following Directory Structures
+// 		In Documents Directory
+// 		A. Create iOSTraining Directory Inside Documents
+// 		B. Create Progress Directory Inside Documents
+
+// 2. Write Following Hello World Program
+	
+	print( "Hello World!" )
+
+// 3. Save Program In Hello.swift In Progress Directory
+
+// 4. Open Command Prompt Application
+// 		cd YOUR_HOME_PATH\Documents\iOSTraining\Progress
+//		dir
+
+// 5. Compile Hello.swift
+// 		swiftc Hello.swift
+
+// 6. Run Compiled Code
+// 		Hello
+
